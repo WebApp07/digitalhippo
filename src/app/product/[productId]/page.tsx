72093ed10d85cf8e3d986156461844d016b9bb6d
unknown <aminezouguig@proton.me>
2024-01-18 16:22:40 +0100

    Done Creating our product detail page

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -3,6 +3,12 @@ import Link from "next/link";
 import {getPayloadClient} from "@/get-payload";
 import {notFound} from "next/navigation";
 import {formatPrice} from "@/lib/utils";
+import {PRODUCT_CATEGORIES} from "@/config";
+import React from "react";
+import {Check, Shield} from "lucide-react";
+import ImageSlider from "@/components/ImageSlider";
+import ProductReel from "@/components/ProductReel";
+import AddToCartButton from "@/components/AddToCartButton";
 
 interface PageProps {
     params: {
@@ -37,6 +43,13 @@ const Page = async ({ params }: PageProps) => {
 
   if (!product) return notFound()
 
+    const label = PRODUCT_CATEGORIES.find(({ value }) => value === product.category)?.label
+
+      const validUrls = product.images
+    .map(({ image }) =>
+      typeof image === 'string' ? image : image.url
+    )
+    .filter(Boolean) as string[]
 
 
     return (
@@ -55,35 +68,90 @@ const Page = async ({ params }: PageProps) => {
                                             className="font-medium text-sm text-muted-foreground hover:text-gray-900">
                                             {breadcrumb.name}
                                         </Link>
-                                      {i !== BREADCRUMBS.length -1 ? (
-                                           <svg
-                                              viewBox='0 0 20 20'
-                                              fill='currentColor'
-                                              aria-hidden='true'
-                                              className='ml-2 h-5 w-5 flex-shrink-0 text-gray-300'>
-                                              <path d='M5.555 17.776l8-16 .894.448-8 16-.894-.448z' />
+                                        {i !== BREADCRUMBS.length - 1 ? (
+                                            <svg
+                                                viewBox='0 0 20 20'
+                                                fill='currentColor'
+                                                aria-hidden='true'
+                                                className='ml-2 h-5 w-5 flex-shrink-0 text-gray-300'>
+                                                <path d='M5.555 17.776l8-16 .894.448-8 16-.894-.448z'/>
                                             </svg>
 
-                                      ): null}
+                                        ) : null}
                                     </div>
                                 </li>
                             ))}
 
                         </ol>
 
-                      <div className="mt-4">
-                        <h1 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>{product.name}</h1>
-                      </div>
-
-                      <section className="mt-4">
-                        <div className="flex items-center">
-                          <p className="font-medium text-gray-900">{formatPrice(product.price)}</p>
+                        <div className="mt-4">
+                            <h1 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>{product.name}</h1>
                         </div>
-                      </section>
 
+                        <section className='mt-4'>
+                            <div className='flex items-center'>
+                                <p className='font-medium text-gray-900'>
+                                    {formatPrice(product.price)}
+                                </p>
+
+                                <div className='ml-4 border-l text-muted-foreground border-gray-300 pl-4'>
+                                    {label}
+                                </div>
+                            </div>
+
+                            <div className='mt-4 space-y-6'>
+                                <p className='text-base text-muted-foreground'>
+                                    {product.description}
+                                </p>
+                            </div>
+
+                            <div className='mt-6 flex items-center'>
+                                <Check
+                                    aria-hidden='true'
+                                    className='h-5 w-5 flex-shrink-0 text-green-500'
+                                />
+                                <p className='ml-2 text-sm text-muted-foreground'>
+                                    Eligible for instant delivery
+                                </p>
+                            </div>
+                        </section>
                     </div>
+                    {/* Product images */}
+                    <div className="mt-10 lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-center">
+                        <div className="aspect-square rounded-lg">
+                            <ImageSlider urls={validUrls} />
+                        </div>
+                    </div>
+
+                    {/* add to cart part */}
+
+                    <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
+                        <div>
+                            <div className="mt-10">
+                                <AddToCartButton  />
+                            </div>
+                            <div className="mt-6 text-center">
+                                <div className="group inline-flex text-sm text-medium">
+                                    <Shield
+                                        aria-hidden='true'
+                                        className='mr-2 h-5 w-5 flex-shrink-0 text-gray-400'
+                                    />
+                                    <span className='text-muted-foreground hover:text-gray-700'>
+                                        30 Day Return Guarantee
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
                 </div>
+                <ProductReel
+                    href='/products'
+                    query={{ category: product.category, limit: 4 }}
+                    title={`Similar ${label}`}
+                    subtitle={`Browse similar high-quality ${label} just like '${product.name}'`}
+                  />
             </MaxWidthWrapper>
     )
 }
--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import Image from "next/image";
 import "swiper/css";
