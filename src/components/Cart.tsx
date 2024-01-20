4f5cb158eed3e6e9deb2730d6588544aee2bc95b
unknown <aminezouguig@proton.me>
2024-01-20 19:33:15 +0100

    creating our checkout page

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -9,12 +9,19 @@ import {buttonVariants} from "./ui/button";
 import {useCart} from "@/hooks/use-cart";
 import {ScrollArea} from "./ui/scroll-area";
 import CartItem from "./cartItem";
+import {useEffect, useState} from "react";
 
 
 
 const Cart = () => {
     const {items} = useCart()
     const itemCount = items.length
+
+    const [isMounted, setIsMounted] = useState<boolean>(false)
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, []);
 
     const cartTotal = items.reduce((total, {product}) => total + product.price, 0)
 
@@ -25,7 +32,7 @@ const Cart = () => {
             <SheetTrigger className="group -m-2 flex items-center p-2">
                 <ShoppingCart aria-hidden="true" className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
                 <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-                    {itemCount}
+                    {isMounted ? itemCount : 0}
                 </span>
             </SheetTrigger>
             <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
